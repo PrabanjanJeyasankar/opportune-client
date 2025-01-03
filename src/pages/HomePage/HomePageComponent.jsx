86207ad5b39696044e69b0cd5df8abdc7b7a020f
prabanjanjeyasankar <prabanjan@gmail.com>
2025-01-03 22:52:57 +0530

    feat: add navigation bar with responsiveness

---
+++ src/pages/HomePage/HomePageComponent.jsx
@@ -0,0 +1,12 @@
+import NavigationBarComponent from '../../components/NavigationBarComponent/NavigationBarComponent'
+import homePageStyles from './HomePageComponent.module.css'
+
+function HomePageComponent() {
+  return (
+    <div>
+      <NavigationBarComponent/>
+    </div>
+  )
+}
+
+export default HomePageComponent
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,13 +3,14 @@ import { Route, Routes } from 'react-router-dom'
 import LoginComponent from '../pages/LoginPageComponent/LoginPageComponent'
 import SignupComponent from '../pages/SignupPageComponent/SignupPageComponent'
 import RequestOtpPageComponent from '../pages/RequestOTPPageComponent/RequestOTPPageComponent'
+import HomePageComponent from '../pages/HomePage/HomePageComponent'
 
 const AppRoutes = () => {
   return (
     <div className=''>
       <Suspense fallback={<div>Loading...</div>}  >
       <Routes>
-        <Route exact path='/' element={<div>Home Page</div>}/>
+        <Route exact path='/' element={<HomePageComponent/>} />
         <Route path='/login' element={<LoginComponent/>}/>
         <Route path='/signup' element={<SignupComponent/>}/>
         <Route path='/request-otp' element={<RequestOtpPageComponent/>}/>
