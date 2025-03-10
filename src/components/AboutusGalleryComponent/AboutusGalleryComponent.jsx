c055565499c703274a14c14e3d85b6d272ea3471
prabanjanjeyasankar <prabanjanjeyasankar@gmail.com>
2025-03-10 20:12:01 +0530

    feat - add responsiveness to about us and footer components.

--- src/components/AboutusGalleryComponent/AboutusGalleryComponent.jsx
+++ src/components/AboutusGalleryComponent/AboutusGalleryComponent.jsx
@@ -1,13 +1,16 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import styles from './AboutusGalleryComponent.module.css'
+import { Skeleton } from '../ui/skeleton'
 
 function AboutusGalleryComponent({ images }) {
     const imagesArray = Object.values(images)
     const galleryRef = useRef(null)
     const columnsRef = useRef([])
+    const [loadedImages, setLoadedImages] = useState({})
 
     useEffect(() => {
         const handleScroll = () => {
+            if (!galleryRef.current) return
             const scrollTop =
                 window.pageYOffset || document.documentElement.scrollTop
             const rect = galleryRef.current.getBoundingClientRect()
@@ -23,21 +26,25 @@ function AboutusGalleryComponent({ images }) {
                 )
             )
 
+            const screenWidth = window.innerWidth
+            const mobileFactor = screenWidth < 768 ? 0.4 : 1
+
             columnsRef.current.forEach((column, index) => {
                 if (!column) return
-                const speed = 1 - index * 1
-                const yMove = -scrollProgress * 150 * speed
-
+                const speed = 1 - index * 0.7
+                const yMove = -scrollProgress * 150 * speed * mobileFactor
                 const unevenMargin = index % 2 === 0 ? index * 40 : index * 20
                 column.style.transform = `translateY(${yMove}px)`
                 column.style.marginTop = `${unevenMargin}px`
             })
         }
 
-        window.addEventListener('scroll', handleScroll)
+        window.addEventListener('scroll', handleScroll, { passive: true })
         handleScroll()
 
-        return () => window.removeEventListener('scroll', handleScroll)
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
     }, [])
 
     const availableCount = imagesArray.length
@@ -47,120 +54,57 @@ function AboutusGalleryComponent({ images }) {
         return imagesArray[actualIndex]
     }
 
+    const handleImageLoad = (index) => {
+        setLoadedImages((prev) => ({ ...prev, [index]: true }))
+    }
+
     return (
         <div className={styles.gallery_container}>
             <div className={styles.gallery_grid} ref={galleryRef}>
                 <div className={styles.gallery_row}>
-                    <div
-                        className={styles.gallery_column}
-                        ref={(el) => (columnsRef.current[0] = el)}>
-                        {[0, 1, 2, 3, 4].map((offset) => {
-                            const image = getImageAtIndex(offset)
-                            return (
-                                <div
-                                    key={`col1-${offset}`}
-                                    className={styles.gallery_item}>
-                                    <img
-                                        src={image.imageUrl}
-                                        alt={
-                                            image.title ||
-                                            image.description ||
-                                            ''
-                                        }
-                                        className={styles.gallery_image}
-                                    />
-                                </div>
-                            )
-                        })}
-                    </div>
-                    <div
-                        className={styles.gallery_column}
-                        ref={(el) => (columnsRef.current[1] = el)}>
-                        {[5, 6, 7, 8, 9].map((offset) => {
-                            const image = getImageAtIndex(offset)
-                            return (
-                                <div
-                                    key={`col2-${offset}`}
-                                    className={styles.gallery_item}>
-                                    <img
-                                        src={image.imageUrl}
-                                        alt={
-                                            image.title ||
-                                            image.description ||
-                                            ''
-                                        }
-                                        className={styles.gallery_image}
-                                    />
-                                </div>
-                            )
-                        })}
-                    </div>
-                    <div
-                        className={styles.gallery_column}
-                        ref={(el) => (columnsRef.current[2] = el)}>
-                        {[10, 11, 12, 13, 14].map((offset) => {
-                            const image = getImageAtIndex(offset)
-                            return (
-                                <div
-                                    key={`col3-${offset}`}
-                                    className={styles.gallery_item}>
-                                    <img
-                                        src={image.imageUrl}
-                                        alt={
-                                            image.title ||
-                                            image.description ||
-                                            ''
-                                        }
-                                        className={styles.gallery_image}
-                                    />
-                                </div>
-                            )
-                        })}
-                    </div>
-                    <div
-                        className={styles.gallery_column}
-                        ref={(el) => (columnsRef.current[3] = el)}>
-                        {[15, 16, 17, 18, 19].map((offset) => {
-                            const image = getImageAtIndex(offset)
-                            return (
-                                <div
-                                    key={`col4-${offset}`}
-                                    className={styles.gallery_item}>
-                                    <img
-                                        src={image.imageUrl}
-                                        alt={
-                                            image.title ||
-                                            image.description ||
-                                            ''
-                                        }
-                                        className={styles.gallery_image}
-                                    />
-                                </div>
-                            )
-                        })}
-                    </div>
-                    <div
-                        className={styles.gallery_column}
-                        ref={(el) => (columnsRef.current[4] = el)}>
-                        {[20, 21, 22, 23, 24].map((offset) => {
-                            const image = getImageAtIndex(offset)
-                            return (
-                                <div
-                                    key={`col5-${offset}`}
-                                    className={styles.gallery_item}>
-                                    <img
-                                        src={image.imageUrl}
-                                        alt={
-                                            image.title ||
-                                            image.description ||
-                                            ''
-                                        }
-                                        className={styles.gallery_image}
-                                    />
-                                </div>
-                            )
-                        })}
-                    </div>
+                    {[...Array(5)].map((_, colIndex) => (
+                        <div
+                            key={colIndex}
+                            className={styles.gallery_column}
+                            ref={(el) => (columnsRef.current[colIndex] = el)}>
+                            {[...Array(5)].map((_, rowIndex) => {
+                                const imageIndex = colIndex * 5 + rowIndex
+                                const image = getImageAtIndex(imageIndex)
+
+                                return (
+                                    <div
+                                        key={`col${colIndex}-${rowIndex}`}
+                                        className={styles.gallery_item}>
+                                        {!loadedImages[imageIndex] && (
+                                            <Skeleton
+                                                className={
+                                                    styles.skeleton_project_card_image
+                                                }
+                                            />
+                                        )}
+                                        <img
+                                            src={image.imageUrl}
+                                            alt={
+                                                image.title ||
+                                                image.description ||
+                                                ''
+                                            }
+                                            className={`${
+                                                styles.gallery_image
+                                            } ${
+                                                loadedImages[imageIndex]
+                                                    ? styles.image_loaded
+                                                    : ''
+                                            }`}
+                                            onLoad={() =>
+                                                handleImageLoad(imageIndex)
+                                            }
+                                        />
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
